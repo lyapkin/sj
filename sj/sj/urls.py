a17945892586bad24f905f13baca026a5be9a100
lyapkin <lyapkin992@gmail.com>
2024-05-08 17:00:11 +0500

    .

--- sj/sj/urls.py
+++ sj/sj/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 ]
@@ -28,3 +30,6 @@ urlpatterns += i18n_patterns(
     path('api/products/', include('products.urls')),
     # prefix_default_language=False
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

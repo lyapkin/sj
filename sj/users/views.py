2cacb403add017f6ca7aa6817ae9ac8e04a78512
lyapkin <lyapkin992@gmail.com>
2024-05-14 16:13:52 +0500

    .

--- sj/users/views.py
+++ sj/users/views.py
@@ -55,7 +55,7 @@ def confirm(request, *args, **kwargs):
 
     login(request, user)
     otp.delete()
-    return Response({'user': UserSerializer(request.user).data, 'cart': CartSerializer(Cart.objects.filter(user=request.user), many=True).data}, status=200)
+    return Response({**UserSerializer(request.user).data, 'cart': CartSerializer(Cart.objects.filter(user=request.user), many=True).data}, status=200)
 
 
 @api_view(['GET'])
@@ -69,4 +69,4 @@ def logout_view(request, *args, **kwargs):
 @permission_classes([IsAuthenticated])
 def check_auth(request, *args, **kwargs):
     # print(Cart.objects.filter(request.user))
-    return Response({'user': UserSerializer(request.user).data, 'cart': CartSerializer(Cart.objects.filter(user=request.user), many=True).data}, status=200)
+    return Response({**UserSerializer(request.user).data, 'cart': CartSerializer(Cart.objects.filter(user=request.user), many=True).data}, status=200)
